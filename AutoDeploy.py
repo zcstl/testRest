cc080407966ce4843b44921ffca849e4e42d6c2c
zcszcs <hnhkzcs@163.com>
2017-06-17 18:17:13 +0800

    Update AutoDeploy.py

--- AutoDeploy.py
+++ AutoDeploy.py
@@ -1,12 +1,15 @@
+# coding=utf-8
 import paramiko
-
+import json
 
 class DeployTemplate(object):
-    def __init__(self, host, user, pwd):
+    def __init__(self, host, user, pwd, params):
         self.host = host
         self.user = user
         self.pwd = pwd
         self.ssh_client = None
+        self.params = params
+
 
     def initSSHConn(self):
         try:
@@ -24,6 +27,38 @@ class DeployTemplate(object):
     def closeSSHConn(self):
         self.ssh_client.close()
 
+    def getCdStr(self):
+        return 'cd ' + self.params.getVal('workDir')
+
+    def generateCmd(self, cmdList):
+        cmd = ''
+        for tt in cmdList:
+            cmd += tt + ';'
+        return cmd
+
+    def isSureRun(self, cmd):
+        respon = raw_input('Sure for running ' + cmd + '? Please input y or n:  ')
+        if respon == 'y':
+            return True
+        elif respon == 'n':
+            return False
+        else:
+            print '***********Please input y or n***********'
+            return self.isSureRun(cmd)
+
+    def runCmdList(self, cmdList):
+        try:
+            cmd = self.generateCmd(cmdList)
+            if self.isSureRun(cmd):
+                pass
+                #stdin, stdout, stderr = self.doCmd(cmd)
+                #print 'The stdout is: ' , stdout.readlines()
+            else:
+                print 'pass this cmd: ' + cmd
+        except BaseException, msg:
+            print msg
+            self.closeSSHConn()
+
     def doBefore(self):
         self.initSSHConn()
 
@@ -38,35 +73,117 @@ class DeployTemplate(object):
         self.mainLogic()
         self.doAfter()
 
-
-class Docker(DeployTemplate):
+class Environment(DeployTemplate):
+    '''负责环境准备，包括文件准备，环境清理'''
     def __init__(self, host, user, pwd):
         super(Docker, self).__init__(host, user, pwd)
 
     def mainLogic(self):
-        stdin, stdout, stderr = self.doCmd("echo tt")
+        cmd = self.getCdStr()
+        stdin, stdout, stderr = self.doCmd(cmd)
         print stdout.readlines()
 
 
+
+class Docker(DeployTemplate):
+    '''负责镜像上传'''
+    def __init__(self, host, user, pwd, params):
+        super(Docker, self).__init__(host, user, pwd, params)
+
+    def mainLogic(self):
+        '''
+        sudo docker load -i /tmp/zcs/cpe-builder-0.3.0.tar.gz
+        sudo docker tag cpe-builder:0.3.0 ip:20202/root/cpe-builder:0.3.0
+        sudo docker push 100.101.58.80:20202/root/cpe-builder:0.3.0
+        '''
+        #imgList = ['website', 'service', 'builder', 'assembling']
+        imgList = ['website', 'builder', 'service', 'assembling']
+        cmdList = []
+        for img in imgList:
+            cmd = 'sudo docker load -i ' + self.params.getVal('imageDir') + '/'+ self.params.getVal('images-' + img + '-fileNmae')
+            cmdList.append(cmd)
+            cmd = 'sudo docker tag ' + self.params.getVal('images-' + img + '-name') + ' ' + self.params.getVal('images-' + img + '-tag')
+            cmdList.append(cmd)
+            cmd = 'sudo docker push ' + self.params.getVal('images-' + img + '-tag')
+            cmdList.append(cmd)
+            self.runCmdList(cmdList)
+
+
 class Chart(DeployTemplate):
+    '''负责chart包上传'''
     def __init__(self):
-        super(Chart, self).__init__();
+        super(Chart, self).__init__()
 
     def mainLogic(self):
         pass
 
 
 class Setup(DeployTemplate):
+    '''负责模板上传堆栈安装'''
     def __init__(self):
-        super(Setup, self).__init__();
+        super(Setup, self).__init__()
 
     def mainLogic(self):
         pass
 
+class ParamsInterface(object):
+    def getVal(self, key):
+        pass
+    def setVal(self, key, val):
+        pass
+
+class Params(ParamsInterface):
+    '''全局参数字典'''
+    def __init__(self, jsonFile):
+        self.paramsDict =self.paraseJsonFile(jsonFile)
+
+    def getVal(self, key):
+        return self.paramsDict[key]
+
+    def setVal(self, key, val):
+        self.paramsDict[key] = val
+
+    def paraseJsonFile(self, jsonFile):
+        try:
+            js = json.load(file(jsonFile))
+            dict = {}
+
+            dict['workDir'] = self.pareseNodePath(js, ['workDir'])
+            dict['imageDir'] = self.pareseNodePath(js, ['imageDir'])
+
+            #docker images paras parese
+            imgList = ['website', 'builder', 'service', 'assembling']
+            for img in imgList:
+                dict['images-' + img + '-fileNmae'] = self.pareseNodePath(js, ['images', img, 'fileName'])
+                dict['images-' + img + '-name'] = self.pareseNodePath(js, ['images', img, 'name']) + ':' + self.pareseNodePath(js, ['images', img, 'tag'])
+                dict['images-' + img + '-tag'] = self.pareseNodePath(js, ['omVip']) + ':' \
+                                             + self.pareseNodePath(js, ['images', 'port']) \
+                                             + '/' + self.pareseNodePath(js, ['images', 'user']) \
+                                             + '/' + dict['images-' + img + '-name']
+            #chart package
+            for img in imgList:
+                pass
+        except ValueError, e:
+            print e
+
+        return dict
+
+
+
+
+    def pareseNodePath(self, js, nodeList):
+        for node in nodeList:
+            js = js.get(node)
+        return js
 
 def main():
-    docker = Docker('192.168.56.101', 'zcsubuntu', 'zcszcszcs')
-    docker.execute()
+    try:
+        params = Params('paramsTmp.json')
+        docker = Docker('100.101.58.105', 'root', 'Huawei@123', params)
+        docker.execute()
+    except BaseException, e:
+        print e
+        docker.closeSSHConn()
 
 if '__name__' == '__main__':
     main()
